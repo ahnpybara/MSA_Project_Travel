e6ae6a646137f0ae1034f857def0900039edd4ff
Seungchan00 <jscpys1@naver.com>
2024-02-21 11:36:31 +0900

    rollback

--- gateway/src/main/java/travel/Application.java
+++ gateway/src/main/java/travel/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class Application {
 
-    public static ApplicationContext applicationContext;
+    public static applicationContext applicationContext;
 
     public static void main(String[] args) {
         applicationContext = SpringApplication.run(Application.class, args);
