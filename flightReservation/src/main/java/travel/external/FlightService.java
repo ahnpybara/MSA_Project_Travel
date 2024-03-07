5cc0f3d55eb5bee11976f3c1b81cca2854d03a60
Seungchan00 <jscpys1@naver.com>
2024-03-08 03:13:48 +0900

    refactoring flightreservation

--- flightReservation/src/main/java/travel/external/FlightService.java
+++ flightReservation/src/main/java/travel/external/FlightService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "screen", url = "${api.url.screen}")// 나중에 리펙토링 제대로 되면 screen 이아니라 flight 로 사용 yml에도 screen->flight 변경
+@FeignClient(name = "flight", url = "${api.url.flight}")// 나중에 리펙토링 제대로 되면 screen 이아니라 flight 로 사용 yml에도 screen->flight 변경
 public interface FlightService {
     @GetMapping(path = "/flights")
     public List<Flight> getFlight();
@@ -16,6 +16,6 @@ public interface FlightService {
     @GetMapping(path = "/flights/{id}")
     public Flight getFlight(@PathVariable("id") Long id);
 
-    @GetMapping(path = "/flights/search")       // 좌석 호출 하기 위해 찾음
+    @GetMapping(path = "/flights/seatCapacity")       // 좌석 호출 하기 위해 찾음
     public ResponseEntity<Flight> searchFlights(@RequestParam("flightId") Long flightId);     
 }
